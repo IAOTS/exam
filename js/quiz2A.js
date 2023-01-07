0816d93bbd0b40fc9ee4ecdc6733afd47fdf606a
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-01-07 23:12:59 +0300

    Update quiz2A.js

--- js/quiz2A.js
+++ js/quiz2A.js
@@ -1,153 +1,229 @@
 //Question ba
 var questionBank = [{
+        main: 'Listening',
+        desc: 'Listen to Paulo and Georgia talking. Choose the correct answers.',
+        question: 'Georgia wishes she _____ . ',
+        option: ['could do a better job', 'had another teacher', 'could be a better writer'],
+        answer: 'had another teacher'
+    },
+    {
+        main: 'Listening',
+        desc: 'Listen to Paulo and Georgia talking. Choose the correct answers.',
+        question: 'Paulo thinks she should _____ .',
+        option: ['write about it', 'change schools', 'talk to her teacher'],
+        answer: 'talk to her teacher'
+    },
+    {
+        main: 'Listening',
+        desc: 'Listen to Dave and Michelle talking. Choose the correct answers.',
+        question: 'Michelle wants _____ .',
+        option: ['to plan a pool party', 'to know who\’s bringing the food', 'to eat some chicken salad'],
+        answer: 'to know who\’s bringing the food'
+    },
+    {
+        main: 'Listening',
+        desc: 'Listen to Dave and Michelle talking. Choose the correct answers.',
+        question: 'Michelle tells Dave to first _____ .',
+        option: ['cut up the chicken', 'cut up the onions and celery', 'bake, barbecue, or broil the chicken'],
+        answer: 'bake, barbecue, or broil the chicken'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation',
+        question: 'Mr. Peterson: Dan, _________ you mind washing the dishes?',
+        option: ['should', 'could', 'would'],
+        answer: 'would'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation',
+        question: 'Dan: Can I do it later? I don\’t _________ to go to practice tonight.',
+        option: ['have', 'must', 'ought'],
+        answer: 'have'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation',
+        question: 'Mr. Peterson: No. You _________ do it now. Our guests are arriving in an hour.',
+        option: ['needs', 'should', 'would'],
+        answer: 'should'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation',
+        question: 'Dan: All right, but _________ you give me a few minutes to finish this video game?',
+        option: ['should', 'ought', 'could'],
+        answer: 'could'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation',
+        question: 'Mr. Peterson: Well, OK, but _________ better start right after that.',
+        option: ['you\’ll', 'you\’d', 'you\’ve'],
+        answer: 'you\’d'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: Would you mind waiting here, please? B: ___________ ',
+        option: ['Sorry. I\’m cold.', 'I’m sorry. I can\’t. I don\’t have time.', 'Yes, I agree. We are!'],
+        answer: 'I’m sorry. I can\’t. I don\’t have time.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: What are you going to do Saturday?     B: ___________ ',
+        option: ['I probably won\’t do anything', 'That doesn\’t sound like much fun.', 'It\’s too early.'],
+        answer: 'I probably won\’t do anything'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: You\’d better take a sweater with you.     B: ___________ ',
+        option: ['This one is better.', 'Thanks. I will.', 'Yes, you should.'],
+        answer: 'Thanks. I will.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: Do you know where the nearest bus stop is?     B: ___________ ',
+        option: ['That\’s great. I really appreciate it.', 'Sure. Would you like to see them?', 'No, I\’m sorry. I don\’t.'],
+        answer: 'No, I\’m sorry. I don\’t.'
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'The city passed a new bill to build several new bike _________ .',
+        option: ['lanes', 'lights',
+            'spaces'
+        ],
+        answer: 'lanes'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'Your boots are wet. _________ before you walk on my clean floor.',
+        option: ['Take them out ', 'Turn them off',
+            'Take them off'
+        ],
+        answer: 'Take them off'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'Molly\’s new apartment is amazing. It\’s so bright and _________ .',
+        option: ['run-down', 'dingy',
+            'modern'
+        ],
+        answer: 'modern'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'When I travel, I don\’t usually bring a large suitcase. I use my _________ instead.',
+        option: ['first-aid kit', 'carry-on bag',
+            'vaccination'
+        ],
+        answer: 'carry-on bag'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'I only _________free apps onto my tablet.',
+        option: ['download', 'back up',
+            'run down'
+        ],
+        answer: 'download'
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Complete this conversation with the simple past or the present perfect of the verbs in parentheses',
+        question: 'Have you ever _________________ the Indian drink chai?',
+        option: ['try', 'tried', 'trying'],
+        answer: 'tried'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete this conversation with the simple past or the present perfect of the verbs in parentheses',
+        question: 'Yes, but I _________________ any for a long time.',
+        option: ['have not had', 'have', 'had'],
+        answer: 'have not had'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete this conversation with the simple past or the present perfect of the verbs in parentheses',
+        question: 'When did you _________________ it',
+        option: ['drink', 'drank', 'drunk'],
+        answer: 'drink'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete this conversation with the simple past or the present perfect of the verbs in parentheses',
+        question: 'Last winter. I _________________ it for my roommate',
+        option: ['make', 'made', 'do'],
+        answer: 'made'
+    },
+
+    {
+        main: 'Read about the Winter Carnival. Then choose T (true) or F (false).',
+        desc: 'Quebec\'s Winter Carnival Quebec\'s Winter Carnival is a special celebration in Canada. This holiday started about 50 years ago and lasts for 17 days each year.It celebrates the sights and sounds of winter.The Winter Carnival has a special character called Bonhomme Carnival.Bonhomme Carnival looks like a snowman, but he is really a man in a snowman\'s costume.Bonhomme Carnival is present at all of the carnival activities, and he plays an important role as the symbol of the celebration.There are many exciting activities at the winter carnival.For example,there are night parades.There is also a snow sculpture display.This display is like an outdoor museum,where artists show sculpture they have made out of snow.Finally,there are many sporting events such as ski races,canoe races,and even a \"snow swim\".The Winter Carnival has become very popular with tourists.Now almost a million people come to this northern city to visit the carnival each year.',
+        question: 'Quebec\’s Winter Carnival started about 17 years ago.',
+        option: ['True', 'False', 'Not mentioned'],
+        answer: 'False'
+    },
+    {
+        main: 'Read about the Winter Carnival. Then choose T (true) or F (false).',
+        desc: 'Quebec\'s Winter Carnival Quebec\'s Winter Carnival is a special celebration in Canada. This holiday started about 50 years ago and lasts for 17 days each year.It celebrates the sights and sounds of winter.The Winter Carnival has a special character called Bonhomme Carnival.Bonhomme Carnival looks like a snowman, but he is really a man in a snowman\'s costume.Bonhomme Carnival is present at all of the carnival activities, and he plays an important role as the symbol of the celebration.There are many exciting activities at the winter carnival.For example,there are night parades.There is also a snow sculpture display.This display is like an outdoor museum,where artists show sculpture they have made out of snow.Finally,there are many sporting events such as ski races,canoe races,and even a \"snow swim\".The Winter Carnival has become very popular with tourists.Now almost a million people come to this northern city to visit the carnival each year.',
+        question: 'Bonhomme Carnaval is a man dressed as a snowman.',
+        option: ['True', 'False', 'Not mentioned'],
+        answer: 'True'
+    },
+    {
+        main: 'Read about the Winter Carnival. Then choose T (true) or F (false).',
+        desc: 'Quebec\'s Winter Carnival Quebec\'s Winter Carnival is a special celebration in Canada. This holiday started about 50 years ago and lasts for 17 days each year.It celebrates the sights and sounds of winter.The Winter Carnival has a special character called Bonhomme Carnival.Bonhomme Carnival looks like a snowman, but he is really a man in a snowman\'s costume.Bonhomme Carnival is present at all of the carnival activities, and he plays an important role as the symbol of the celebration.There are many exciting activities at the winter carnival.For example,there are night parades.There is also a snow sculpture display.This display is like an outdoor museum,where artists show sculpture they have made out of snow.Finally,there are many sporting events such as ski races,canoe races,and even a \"snow swim\".The Winter Carnival has become very popular with tourists.Now almost a million people come to this northern city to visit the carnival each year.',
+        question: 'The snow sculpture display takes place indoors.',
+        option: ['True', 'False', 'Not mentioned'],
+        answer: 'False'
+    },
+    {
+        main: 'Read about the Winter Carnival. Then choose T (true) or F (false).',
+        desc: 'Quebec\'s Winter Carnival Quebec\'s Winter Carnival is a special celebration in Canada. This holiday started about 50 years ago and lasts for 17 days each year.It celebrates the sights and sounds of winter.The Winter Carnival has a special character called Bonhomme Carnival.Bonhomme Carnival looks like a snowman, but he is really a man in a snowman\'s costume.Bonhomme Carnival is present at all of the carnival activities, and he plays an important role as the symbol of the celebration.There are many exciting activities at the winter carnival.For example,there are night parades.There is also a snow sculpture display.This display is like an outdoor museum,where artists show sculpture they have made out of snow.Finally,there are many sporting events such as ski races,canoe races,and even a \"snow swim\".The Winter Carnival has become very popular with tourists.Now almost a million people come to this northern city to visit the carnival each year.',
+        question: 'About a million tourists visit the carnival each year.',
+        option: ['True', 'False', 'Not mentioned'],
+        answer: 'True'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'Remember _________ the new software tomorrow.',
+        option: ['download', 'downloading', 'to download'],
+        answer: 'to download'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'Do you know how often _________ texts to your friends?',
+        option: ['do you send', 'send', 'you send'],
+        answer: 'you send'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'I wish I _________ get better cell-phone service here.',
+        option: ['will', 'could', 'can'],
+        answer: 'could'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: 'This file is used _________ business reports.',
+        option: ['prepare', 'to prepare', 'preparing'],
+        answer: 'to prepare'
+    },
 
 
-        main: 'Listening',
-        desc: ' Listen. Write the words you hear.',
-        audio: '',
-        question: 'Is your_____________there?',
-        option: [' they\'re', 'there are', ' their', 'they are'],
-        answer: 'they are'
-    },
-    {
-        main: 'Listening',
-        desc: '',
-        audio: '',
-        question: ' Where’s the_______________ ?.',
-        option: ['on', 'at', 'in', 'next'],
-        answer: 'at'
-    },
-    {
-        main: 'Listening',
-        desc: '',
-        audio: '',
-        question: 'Are your_________ here?',
-        option: ['And', 'So', 'Also', 'Too'],
-        answer: 'So'
-    },
-
-    {
-        main: 'Listening',
-        desc: '',
-        audio: '',
-        question: 'I have a bad_______________ .',
-        option: ['watching', 'watch', ' to watch', 'watched'],
-        answer: 'watching'
-    },
-    {
-        main: 'Listening',
-        desc: '',
-        audio: '',
-        question: 'I sprained my__________________.',
-        option: [' The most ', 'Almost', 'All', 'Most of'],
-        answer: 'Almost'
-    },
-    {
-        main: 'Listening',
-        desc: 'Listen. Circle the letter of the sentences you hear.',
-        audio: '',
-        question: '1',
-        option: [' It\’s on the sofa.', 'It\’s under the shelf.', 'It\’s on the shelf', 'excitement'],
-        answer: 'excitement'
-    },
-    {
-        main: 'Listening',
-        desc: 'Listen. Circle the letter of the sentences you hear.',
-        audio: '',
-        question: '2',
-        option: [' I have a fever.', ' She has the flu.', ' I have the flu.'],
-        answer: 'to see'
-    },
-    {
-        main: 'Listening',
-        desc: 'Listen. Circle the letter of the sentences you hear.',
-        audio: '',
-        question: '3',
-        option: ['Yes\, she is.', 'Yes\, it is.', 'Yes\, they are.'],
-        answer: 'the cheapest'
-    },
-    {
-        main: 'Listening',
-        desc: 'Listen. Circle the letter of the sentences you hear.',
-        audio: '',
-        question: '4',
-        option: [' No\, they aren\’t.', 'Yes\, they are.', 'Yes\, we are.'],
-        answer: 'the cheapest'
-    },
-    {
-        main: 'Listening',
-        desc: 'Listen. Circle the letter of the sentences you hear.',
-        audio: '',
-        question: '5',
-        option: ['He\’s working', 'She\’s working.', 'They\’re working.'],
-        answer: 'the cheapest'
-    },
-
-    {
-        main: 'Grammar',
-        desc: 'Choose the correct word.',
-        audio: '',
-        question: '_________he from Russia?',
-        option: ['Does', 'Is', ''],
-        answer: 'Does'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Choose the correct word.',
-        audio: '',
-        question: ' The maps are______the table.',
-        option: ['on', 'in', ''],
-        answer: 'on'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Choose the correct word.',
-        audio: '',
-        question: 'Are you_________?',
-        option: ['study', 'studying', 'studies'],
-        answer: 'studying'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Choose the correct word.',
-        audio: '',
-        question: '_________Mrs. Jones have a backache?',
-        option: ['Do', 'Does', 'Did'],
-
-        answer: 'Does'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Choose the correct word.',
-        audio: '',
-        question: ' Go two blocks.____________left.',
-        option: ['Turn', 'Turning', 'Turned'],
-        answer: 'Turn'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Match the questions with the answers',
-        audio: '',
-        question: 'Where are they from?______',
-        option: ['They\'re on the table', 'They\'re from china', 'Yes\,they do.'],
-        answer: 'They\'re from china'
-    },
-    {
-        main: 'Grammar',
-        desc: 'Match the questions with the answers',
-        audio: '',
-        question: 'Are they eating?______',
-        option: ['They\'re on the table', 'They\'re from china', 'Yes\,they are.'],
-        answer: 'Yes\,they are.'
-    },
-    {
-        main: 'Grammar',
-        desc: 'where are the erasers?______',
-        audio: '',
-        question: 'Are they eating?______',
-        option: ['They\'re on the table', 'They\'re from china', 'Yes\,they are.'],
-        answer: 'They\'re on the table'
-    }
 ]
 
 var question = document.getElementById('question');
@@ -180,7 +256,7 @@ function displayQuestion() {
     desc.innerHTML = '' + questionBank[i].desc;
     audio.innerHTML = '' + questionBank[i].audio;
     question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
-    if (i === 1) {
+    if (i === 4) {
         audio1.style.display = "none";
     }
     option0.innerHTML = questionBank[i].option[0];
@@ -218,7 +294,7 @@ function nextQuestion() {
 function previousQuestion() {
     if (i <= questionBank.length - 1) {
         i = i - 1;
-        if (i === 0) {
+        if (i <= 4) {
             audio1.style.display = "block";
         }
         displayQuestion();
@@ -270,6 +346,8 @@ function send_handle() {
     let name = document.getElementById("name").value;
     if (name == "") {
         msg1.innerHTML = "لا يمكنك ترك الاسم فارغا";
+                    parent.location = "index2A.html?#scorecard";
+
     } else {
         var win = open(`https://wa.me/${num}?text=أنا الطالب : ${name}%20كود الإختبار هو :9582${msg}_IAOTS`, 'self');
         // win.focus();
