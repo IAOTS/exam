2c6e03dfdb84e813b3432ebefc6036d315578617
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-09 01:12:10 +0300

    Update homeAccessB.js

--- js/homeAccessB.js
+++ js/homeAccessB.js
@@ -5,7 +5,7 @@
        btn.onclick = function() {
            let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
            // we check if the name is falsey:
-           if (name == "fa21.") {
+           if (name == "AB_pass/test") {
                parent.location = 'indexAccessB.html';
 
            } else {
