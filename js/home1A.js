1b78d4da6a7d33137fa4e0e5b1b9ce1a084436de
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-09 01:09:48 +0300

    Update home1A.js

--- js/home1A.js
+++ js/home1A.js
@@ -2,7 +2,7 @@
        btn.onclick = function() {
            let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
            // we check if the name is falsey:
-           if (name == "fa21.") {
+           if (name == "1A_pass/test") {
                parent.location = 'index1A.html';
 
            } else {
