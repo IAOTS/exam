be20bfd0b588a30089bc2724524dbb6120a2e098
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-08 19:50:16 +0300

    Update quiz1B.js

--- js/quiz1B.js
+++ js/quiz1B.js
@@ -1,1 +1,439 @@
+//Question ba
+var questionBank = [{
 
+
+        main: 'Listening',
+        desc: ' Listen to the people talking. Choose the correct answer.',
+        question: 'The woman spells her name _____',
+        option: ['L-O-U-I-S-A', ' L-U-I-S-A', 'L-U-S-I-A'],
+        answer: 'L-U-S-I-A'
+    },
+    {
+        main: 'Listening',
+        desc: ' Listen to the people talking. Choose the correct answer.',
+        question: ' Her name is _____ .',
+        option: ['Spanish', 'American', 'Italian'],
+        answer: 'Italian'
+    },
+    {
+        main: 'Listening',
+        desc: ' Listen to the people talking. Choose the correct answer.',
+        question: 'There\’s no _____ in the kitchen.',
+        option: ['stove', 'microwave oven', 'refrigerator'],
+        answer: 'microwave oven'
+    },
+
+    {
+        main: 'Listening',
+        desc: ' Listen to the people talking. Choose the correct answer.',
+        question: 'Michael\’s apartment doesn\’t have _____ .',
+        option: ['a view', 'a yard', 'a nice kitchen'],
+        answer: 'a view'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete the conversation. Use the correct form of be.',
+        question: 'Jack: Excuse me. _________________ you Isabel?',
+        option: ['Are', 'Am', 'Is'],
+        answer: 'Are'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete the conversation. Use the correct form of be.',
+        question: 'Isabel: Yes, I _________________ .',
+        option: ['is', 'am', 'are'],
+        answer: 'am'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete the conversation. Use the correct form of be.',
+        question: 'Jack: _________________ your family from Mexico?',
+        option: ['Is', 'Am', 'Are'],
+        answer: 'Is'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete the conversation. Use the correct form of be.',
+        question: 'Isabel: No. We _________________ from Brazil.',
+        option: ['are', 'is', 'am'],
+        answer: 'are'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Complete the conversation. Use the correct form of be.',
+        question: 'Jack: What _________________ your first language, Isabel?',
+        option: ['am', 'are', 'is'],
+        answer: 'is'
+
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the sentences.',
+        question: 'Robert is handsome. He\’s very _________ .',
+        option: ['friendly', 'good-looking', 'serious'],
+        answer: 'good-looking'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the sentences.',
+        question: 'Anne works in a store, and she handles money. She\’s a _________ .',
+        option: ['cashier', 'pilot', 'musician'],
+        answer: 'cashier'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the sentences.',
+        question: 'Mia doesn\’t like her job. It\’s very _________ .',
+        option: ['interesting', 'exciting', 'boring'],
+        answer: 'boring'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the sentences.',
+        question: 'Mario is Celia\’s father. Celia is Mario\’s _________ .',
+        option: ['wife', 'daughter', 'sister'],
+        answer: 'daughter'
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words.',
+        question: 'It\’s cold today, _______________ it\’s very windy.',
+        option: ['and', 'but', 'so'],
+        answer: 'and'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words.',
+        question: 'It\’s noon, ________ we\’re eating lunch.',
+        option: ['and', 'but', 'so'],
+        answer: 'so'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words.',
+        question: 'John is wearing shoes, ________ he isn\’t wearing socks.',
+        option: ['and', 'but', 'so'],
+        answer: 'but'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the missing questions.',
+        question: 'A:______________________________________ ?B: I\’m fine,thanks.',
+        option: ['How are you?', 'How old are you ?', 'Where are you ?'],
+        answer: 'How are you?'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the missing questions.',
+        question: 'A:______________________________________ ?B: I\’m 28 years old.',
+        option: ['How are you?', 'How old are you?', 'How many brothers do you have?'],
+        answer: 'How old are you?'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the missing questions.',
+        question: 'A:______________________________________ ?B: I\’m a salesperson.',
+        option: ['What does your sister do?',
+            'What do you do?',
+            'Where are you from?'
+        ],
+        answer: 'What do you do?'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the missing questions.',
+        question: 'A:______________________________________ ?B: I work in shoe store.',
+        option: [
+            'Where do you work?',
+            'When do you work ?',
+            'How do you work ?'
+        ],
+        answer: 'Where do you work?'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct responses',
+        question: 'A: Have a nice day. B: __________',
+        option: [
+            'It\’s nice to meet you.',
+            'Not bad, thanks.',
+            'OK. See you later.'
+        ],
+        answer: 'OK. See you later.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct responses',
+        question: 'A: What\’s Marta like? B: __________',
+        option: [
+            'She\’s from Mexico.',
+            'She\’s very nice.',
+            'She\’s eighteen.'
+        ],
+        answer: 'She\’s very nice.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct responses',
+        question: 'A: I need a pen. B: __________',
+        option: [
+            'Let me see. Yes, it is.',
+            'No problem. Thank you.',
+            'There\’s a pen on the desk.'
+        ],
+        answer: 'There’s a pen on the desk.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct responses',
+        question: 'A: When do you study?        B: __________ ',
+        option: [
+            'Yes, a lot.',
+            'At school.',
+            'In the evening.'
+        ],
+        answer: 'In the evening.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct responses',
+        question: 'A: Where are my keys? B: __________ ',
+        option: [
+            'They\’re under your hat.',
+            'It\’s on your briefcase.',
+            'There are three.'
+        ],
+        answer: 'They\’re under your hat.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct form of the verb to complete the conversations.',
+        question: 'A: Where do you live?  B: I _____________ in the suburbs with my family.',
+        option: [
+            'live',
+            'lives',
+            'living'
+        ],
+        answer: 'live'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct form of the verb to complete the conversations.',
+        question: 'A: Do you _____________ any children?  B: Yes,we have two children.',
+        option: [
+            'has',
+            'have',
+            'are'
+        ],
+        answer: 'have'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct form of the verb to complete the conversations.',
+        question: 'A: Do you drive to work?  B: No,I _____________.I take the bus.',
+        option: [
+            'do not',
+            'does not',
+            'am not'
+        ],
+        answer: 'do not'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct form of the verb to complete the conversations.',
+        question: 'A: Does your wife _____________ ? B: Yes,she works in a hospital.',
+        option: [
+            'work',
+            'works',
+            'working'
+        ],
+        answer: 'work'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct form of the verb to complete the conversations.',
+        question: 'A: What does she _____________ ?B: She\’ s a nurse.',
+        option: [
+            'do',
+            'does',
+            'doing'
+        ],
+        answer: 'do'
+    },
+]
+
+var question = document.getElementById('question');
+var quizContainer = document.getElementById('quiz-container');
+var scorecard = document.getElementById('scorecard');
+var main = document.getElementById('main');
+var desc = document.getElementById('desc');
+
+var option0 = document.getElementById('option0');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
+var next = document.querySelector('.next');
+var previous = document.querySelector('.previous');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var stat = document.querySelector('#stat');
+var AudioA = document.getElementById('listeningA');
+var i = 0;
+var score = 0;
+
+//function to display questions
+function displayQuestion() {
+
+    for (var a = 0; a < span.length; a++) {
+        span[a].style.background = 'none';
+    }
+
+    main.innerHTML = '' + questionBank[i].main;
+    desc.innerHTML = '' + questionBank[i].desc;
+    question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
+    if (i === 4) {
+        AudioA.style.display = "none";
+
+    }
+    option0.innerHTML = questionBank[i].option[0];
+    option1.innerHTML = questionBank[i].option[1];
+    option2.innerHTML = questionBank[i].option[2];
+    stat.innerHTML = "Question" + ' ' + (i + 1) + ' ' + 'of' + ' ' + questionBank.length;
+
+}
+
+//function to calculate scores
+function calcScore(e) {
+    if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
+        score = score + 1;
+
+    }
+    setTimeout(nextQuestion, 300);
+}
+//function to display next question
+function nextQuestion() {
+
+    if (i < questionBank.length - 1) {
+        i = i + 1;
+        displayQuestion();
+
+    } else {
+        points.innerHTML = score;
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
+    }
+
+
+}
+
+//function to display previous question
+function previousQuestion() {
+    if (i <= questionBank.length - 1) {
+        i = i - 1;
+        if (i <= 4) {
+            AudioA.style.display = "block";
+        }
+        displayQuestion();
+    }
+}
+//click events to previous button
+previous.addEventListener('click', previousQuestion);
+//click events to next button
+next.addEventListener('click', nextQuestion);
+
+
+
+
+
+
+displayQuestion();
+
+audio.onclick = function() {
+    window.addEventListener("click", function() {
+
+        document.querySelector(".iframe").style.display = "block";
+        document.querySelector("#close").style.display = "block";
+
+
+    });
+
+
+
+}
+var btn = document.getElementById("close");
+
+btn.onclick = function() {
+
+    window.addEventListener("click", function() {
+
+        document.querySelector(".iframe").style.display = "none";
+
+        document.querySelector("#close").style.display = "none";
+
+    });
+}
+
+function send_handle() {
+
+    let num = document.getElementById("number").value;
+    let msg = document.getElementById("msg").value;
+    let msg1 = document.getElementById("msg1");
+    msg = points.innerHTML;
+    let name = document.getElementById("name").value;
+    if (name == "") {
+        msg1.innerHTML = "لا يمكنك ترك الاسم فارغا";
+        parent.location = "index1A.html?#scorecard";
+    } else {
+        var win = open(`https://wa.me/${num}?text=أنا الطالب : ${name}%20كود الإختبار هو :9582${msg}_IAOTS`, 'self');
+        // win.focus();
+    }
+}
+
+window.onbeforeunload = function() {
+    return "Dude, are you sure you want to leave? Think of the kittens!";
+}
+
+var sec = 3600,
+    countDiv = document.getElementById("timer"),
+    secpass,
+    countDown = setInterval(function() {
+        'use strict';
+
+        secpass();
+    }, 1000);
+
+function secpass() {
+    'use strict';
+
+    var min = Math.floor(sec / 60),
+        remSec = sec % 60;
+
+    if (remSec < 10) {
+
+        remSec = '0' + remSec;
+
+    }
+    if (min < 10) {
+
+        min = '0' + min;
+
+    }
+    countDiv.innerHTML = min + ":" + remSec;
+
+    if (sec > 0) {
+
+        sec = sec - 1;
+
+    } else {
+
+        clearInterval(countDown);
+
+        countDiv.innerHTML = 'countdown done';
+
+    }
+}
