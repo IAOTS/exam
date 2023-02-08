9037aad843ae95af7a8cfb7da0cc2911cc657c93
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-09 00:19:42 +0300

    Update homeAccessB.js

--- js/homeAccessB.js
+++ js/homeAccessB.js
@@ -1,1 +1,17 @@
 
+
+
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "fa21.") {
+               parent.location = 'indexAccessB.html';
+
+           } else {
+               parent.location = 'homeAccessB.html';
+
+           }
+
+       }
+    
