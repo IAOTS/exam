4448fedf03143d07cc4e37c40db7044d736ea228
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-08 22:10:35 +0300

    Update home2B.js

--- js/home2B.js
+++ js/home2B.js
@@ -1,1 +1,16 @@
 
+
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "fa21.") {
+               parent.location = 'index2B.html';
+
+           } else {
+               parent.location = 'home2B.html';
+
+           }
+
+       }
+    
