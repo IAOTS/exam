8d4c33a918d9b281e8a4688e218d3e507f621bf4
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-08 19:48:57 +0300

    Update home1B.js

--- js/home1B.js
+++ js/home1B.js
@@ -1,1 +1,20 @@
 
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "fa21.") {
+               parent.location = 'index1A.html';
+
+           } else {
+               parent.location = 'home1A.html';
+
+           }
+
+       }
+       var note = document.getElementById("TestNotes");
+       note.onclick = function() {
+           document.getElementById("notes").style.display = "block";
+
+
+       }
