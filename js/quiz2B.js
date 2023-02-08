e767fa9aa945ff3e3f332d6f70725b5c7a8960f3
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-02-08 22:02:31 +0300

    Update quiz2B.js

--- js/quiz2B.js
+++ js/quiz2B.js
@@ -1,1 +1,402 @@
 
+//Question ba
+var questionBank = [{
+
+
+        main: 'Listening',
+        desc: 'Listen to Connor and Keiko talking. Choose the correct answers.',
+        question: 'Keiko has been _____ . ',
+        option: ['making a movie', 'working', 'reading thrillers'],
+        answer: 'working'
+    },
+    {
+        main: 'Listening',
+        desc: 'Listen to Connor and Keiko talking. Choose the correct answers.',
+        question: 'Chase Taylor is _____ .',
+        option: ['an actor', 'a scientist', 'director'],
+        answer: 'an actor'
+    },
+    {
+        main: 'Listening',
+        desc: 'Listen to Austin and Teresa talking. Choose the correct answers.',
+        question: 'Austin\’s friend wanted Austin to _____ . ',
+        option: ['put together a science project for him', 'give him a science project', 'take the project to the teacher'],
+        answer: 'give him a science project'
+    },
+
+    {
+        main: 'Listening',
+        desc: 'Listen to Austin and Teresa talking. Choose the correct answers.',
+        question: 'Teresa said she would _____ .',
+        option: ['think about it', 'give the friend the project', 'refuse to help him'],
+        answer: 'refuse to help him'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation.',
+        question: 'A: This hiking trail is so beautiful. I really enjoy _________ my dogs here.',
+        option: ['walk', ' to walk', 'walking'],
+        answer: 'walking'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation.',
+        question: 'B: So _________ . Uh-oh. There’s a sign.',
+        option: ['am I', 'do I', 'can I'],
+        answer: 'do I'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation.',
+        question: 'A: It _________ be new. What does it mean?',
+        option: ['must', 'should', 'will'],
+        answer: 'must'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation.',
+        question: 'B: I think it means you _________ to walk dogs here.',
+        option: ['can\’t', 'aren\’t allowed', 'don\’t have l'],
+        answer: 'aren\’t allowed'
+
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct words to complete the conversation.',
+        question: 'A: Oh, no. I guess we _________ gone to the park after all.',
+        option: ['would have', 'should have', 'must have'],
+        answer: 'should have'
+
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '1. If I moved to the suburbs, _______',
+        option: ['I\’d feel sad.', 'I\’ll take some aspirin.', 'I\’d have to learn how to drive.'],
+        answer: 'I\’d have to learn how to drive.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '2. If I get a high-paying job, _______',
+        option: ['I\’ll save more money.', 'I\’d have to learn how to drive.', 'I won\’t have to go to summer school.'],
+        answer: 'I\’ll save more money.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '3. If I pass the exam, _______',
+        option: ['I\’ll save more money.', 'I\’d have to learn how to drive.', 'I won\’t have to go to summer school.'],
+        answer: 'I won\’t have to go to summer school.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '4. If I found a million dollars, _______',
+        option: ['I\’ll save more money.', 'I\’d go straight to the police.', 'I won\’t have to go to summer school.'],
+        answer: 'I\’d go straight to the police.'
+    },
+
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '5. If my boyfriend wanted to break up with me, _______',
+        option: ['I\’d feel sad.', 'I\’ll take some aspirin.', 'I\’d go straight to the police.'],
+        answer: 'I\’d feel sad.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct clauses to make logical sentences.',
+        question: '6. If I get a headache, _______',
+        option: [
+            'I\’d feel sad.',
+            'I\’ll take some aspirin.',
+            'I\’d go straight to the police.'
+        ],
+        answer: 'I\’ll take some aspirin.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: What have you been doing recently?    B: ______________',
+        option: [
+            'Great, thanks. And you?',
+            'I\’ve been looking for a job.',
+            'I’ve eaten ice cream.'
+        ],
+        answer: 'I\’ve been looking for a job.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: I can\’t stand hot weather.   B: ______________',
+        option: [
+            'Neither am I.',
+            'Well, so I am.',
+            'Really? I don\’t mind.'
+        ],
+        answer: 'Really? I don\’t mind.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: What would you do if you broke your friend\’s cell phone?    B: ______________',
+        option: [
+            'I would have given it away.',
+            'I don\’t know. What would you do?',
+            'I\’ll probably buy another cell phone.'
+        ],
+        answer: 'I don\’t know. What would you do?'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: What do you think that gesture means?  B: ______________',
+        option: [
+            'It could mean \“good-bye.\”',
+            'Yes, it probably does.',
+            'Oh, that\’s what it means!'
+        ],
+        answer: 'It could mean \“good-bye.\”'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Read each conversation. Choose the correct response.',
+        question: 'A: What kinds of hobbies did you have as a child?  B: ______________',
+        option: [
+            'I\’ve been reading a lot recently.',
+            'I used to play video games.',
+            'I might collect comic books.'
+        ],
+        answer: 'I used to play video games.'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: '1. Tom isn’t blond. His hair is very _____ .',
+        option: [
+            'straight',
+            'dark',
+            'short'
+        ],
+        answer: 'dark'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: '2. I went to the drugstore yesterday and bought a tube of _____ .',
+        option: [
+            'tissues',
+            'cough drops',
+            'toothpaste'
+        ],
+        answer: 'toothpaste'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: '3. Martin likes to go away on weekends. Last Saturday, he _____ his truck to the beach.',
+        option: [
+            'rode',
+            'drove',
+            'lost'
+        ],
+        answer: 'drove'
+    },
+    {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: '4. I\’d like to see some of our friends. Let\’s invite Rick and Simone over to our house and have a _____ .',
+        option: [
+            'musical',
+            'barbecue',
+            'film festival'
+        ],
+        answer: 'barbecue'
+    },
+       {
+        main: 'Grammar',
+        desc: 'Choose the correct word or phrase to complete each sentence.',
+        question: '5. I wanted to change how I look, so I _____ .',
+        option: [
+            'got a credit card',
+            'started a new hobby',
+            'grew a beard'
+        ],
+        answer: 'grew a beard'
+    },
+]
+
+var question = document.getElementById('question');
+var quizContainer = document.getElementById('quiz-container');
+var scorecard = document.getElementById('scorecard');
+var main = document.getElementById('main');
+var desc = document.getElementById('desc');
+
+var option0 = document.getElementById('option0');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
+var next = document.querySelector('.next');
+var previous = document.querySelector('.previous');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var stat = document.querySelector('#stat');
+var AudioA = document.getElementById('listeningA');
+var i = 0;
+var score = 0;
+
+//function to display questions
+function displayQuestion() {
+
+    for (var a = 0; a < span.length; a++) {
+        span[a].style.background = 'none';
+    }
+
+    main.innerHTML = '' + questionBank[i].main;
+    desc.innerHTML = '' + questionBank[i].desc;
+    question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
+    if (i === 4) {
+        AudioA.style.display = "none";
+
+    }
+    option0.innerHTML = questionBank[i].option[0];
+    option1.innerHTML = questionBank[i].option[1];
+    option2.innerHTML = questionBank[i].option[2];
+    stat.innerHTML = "Question" + ' ' + (i + 1) + ' ' + 'of' + ' ' + questionBank.length;
+
+}
+
+//function to calculate scores
+function calcScore(e) {
+    if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
+        score = score + 1;
+
+    }
+    setTimeout(nextQuestion, 300);
+}
+//function to display next question
+function nextQuestion() {
+
+    if (i < questionBank.length - 1) {
+        i = i + 1;
+        displayQuestion();
+
+    } else {
+        points.innerHTML = score;
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
+    }
+
+
+}
+
+//function to display previous question
+function previousQuestion() {
+    if (i <= questionBank.length - 1) {
+        i = i - 1;
+        if (i <= 4) {
+            AudioA.style.display = "block";
+        }
+        displayQuestion();
+    }
+}
+//click events to previous button
+previous.addEventListener('click', previousQuestion);
+//click events to next button
+next.addEventListener('click', nextQuestion);
+
+
+
+
+
+
+displayQuestion();
+
+audio.onclick = function() {
+    window.addEventListener("click", function() {
+
+        document.querySelector(".iframe").style.display = "block";
+        document.querySelector("#close").style.display = "block";
+
+
+    });
+
+
+
+}
+var btn = document.getElementById("close");
+
+btn.onclick = function() {
+
+    window.addEventListener("click", function() {
+
+        document.querySelector(".iframe").style.display = "none";
+
+        document.querySelector("#close").style.display = "none";
+
+    });
+}
+
+function send_handle() {
+
+    let num = document.getElementById("number").value;
+    let msg = document.getElementById("msg").value;
+    let msg1 = document.getElementById("msg1");
+    msg = points.innerHTML;
+    let name = document.getElementById("name").value;
+    if (name == "") {
+        msg1.innerHTML = "لا يمكنك ترك الاسم فارغا";
+        parent.location = "index1A.html?#scorecard";
+    } else {
+        var win = open(`https://wa.me/${num}?text=أنا الطالب : ${name}%20كود الإختبار هو :9582${msg}_IAOTS`, 'self');
+        // win.focus();
+    }
+}
+
+window.onbeforeunload = function() {
+    return "Dude, are you sure you want to leave? Think of the kittens!";
+}
+
+var sec = 3600,
+    countDiv = document.getElementById("timer"),
+    secpass,
+    countDown = setInterval(function() {
+        'use strict';
+
+        secpass();
+    }, 1000);
+
+function secpass() {
+    'use strict';
+
+    var min = Math.floor(sec / 60),
+        remSec = sec % 60;
+
+    if (remSec < 10) {
+
+        remSec = '0' + remSec;
+
+    }
+    if (min < 10) {
+
+        min = '0' + min;
+
+    }
+    countDiv.innerHTML = min + ":" + remSec;
+
+    if (sec > 0) {
+
+        sec = sec - 1;
+
+    } else {
+
+        clearInterval(countDown);
+
+        countDiv.innerHTML = 'countdown done';
+
+    }
+}
