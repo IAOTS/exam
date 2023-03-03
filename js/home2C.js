47ed2c1a91ed2d4799b6f29e6fd59b27a336b55c
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-03-03 21:16:09 +0300

    Update home2C.js

--- js/home2C.js
+++ js/home2C.js
@@ -1,1 +1,15 @@
 
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "2C_pass/test") {
+               parent.location = 'index2C.html';
+
+           } else {
+               parent.location = 'home2C.html';
+
+           }
+
+       }
+    
