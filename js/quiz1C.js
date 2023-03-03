f0f213bcf4d509f52a70ad0aa244a24e4dda4e60
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-03-04 01:25:31 +0300

    Update quiz1C.js

--- js/quiz1C.js
+++ js/quiz1C.js
@@ -241,8 +241,8 @@ function displayQuestion() {
     desc.innerHTML = '' + questionBank[i].desc;
     question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
     if (i === 20) {
-        option2.style.display = "none";
-        option3.style.display = "none";
+   option2.style.visibility = "hidden";
+  option3.style.visibility = "hidden";
 
     }
     option0.innerHTML = questionBank[i].option[0];
@@ -282,8 +282,9 @@ function previousQuestion() {
     if (i <= questionBank.length - 1) {
         i = i - 1;
         if (i < 20) {
-  option2.style.display = "block";
-        option3.style.display = "block";        }
+  option2.style.visibility = "visible";
+  option3.style.visibility = "visible";
+           }
         displayQuestion();
     }
 }
