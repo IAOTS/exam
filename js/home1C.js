f4898ed5a6df85afb093832d735c403e043d79eb
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-03-03 21:14:01 +0300

    Update home1C.js

--- js/home1C.js
+++ js/home1C.js
@@ -1,1 +1,16 @@
 
+
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "1B_pass/test") {
+               parent.location = 'index1C.html';
+
+           } else {
+               parent.location = 'home1C.html';
+
+           }
+
+       }
+    
