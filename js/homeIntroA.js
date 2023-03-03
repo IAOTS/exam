830cb7877f5c04727909a3b2c34ae07b317db73d
IAOTS <121063496+IAOTS@users.noreply.github.com>
2023-03-03 21:15:20 +0300

    Update homeIntroA.js

--- js/homeIntroA.js
+++ js/homeIntroA.js
@@ -1,1 +1,16 @@
 
+
+       var btn = document.getElementById("btn");
+       btn.onclick = function() {
+           let name = prompt("Enter The Password"); // name won't be null, but it may be falsey, so here
+           // we check if the name is falsey:
+           if (name == "IntroA_pass/test") {
+               parent.location = 'indexIntroA.html';
+
+           } else {
+               parent.location = 'homeIntroA.html';
+
+           }
+
+       }
+    
